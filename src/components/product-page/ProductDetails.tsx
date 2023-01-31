99fc8af51fcfdbe50d4e2fbce6e0adf604af58ff
Tanmay Patel <tanmay.1rivet@gmail.com>
2023-01-31 19:03:58 +0530

    basic structure of the product page

---
+++ src/components/product-page/ProductDetails.tsx
@@ -0,0 +1,9 @@
+function ProductDetails() {
+    return (
+        <div>
+            product details
+        </div>
+    )
+};
+
+export default ProductDetails;
--- src/pages/ProductPage.tsx
+++ src/pages/ProductPage.tsx
@@ -1,8 +1,20 @@
+import ProductDetails from "../components/product-page/ProductDetails";
+import ProductImage from "../components/product-page/ProductImage";
+
 function ProductPage() {
     return (
-        <div>
-            Product page
-        </div>
+        <section className="section">
+            <div className="container">
+                <div className="row justify-content-between">
+                    <div className="product-page-col">
+                        <ProductImage />
+                    </div>
+                    <div className="product-page-col">
+                        <ProductDetails />
+                    </div>
+                </div>
+            </div>
+        </section>
     )
 };
 
