c0a2143d382393e58b62c25ae0e38d6881a47e05
Tanmay Patel <tanmay.1rivet@gmail.com>
2023-02-01 18:33:09 +0530

    product image change by clicking on the thumbnails

--- src/components/product-page/ProductDetails.tsx
+++ src/components/product-page/ProductDetails.tsx
@@ -1,21 +1,21 @@
 import minusImage from '../../assets/images/icon-minus.svg'
 import plusImage from '../../assets/images/icon-plus.svg'
-import cartImage from '../../assets/images/icon-cart.svg'
+import cartImage from '../../assets/images/icon-cart-white.svg'
 
 function ProductDetails() {
     return (
         <div className="p-3">
-            <h6 className="product-pre-title">Sneaker Company</h6>
+            <h6 className="product-subtitle">Sneaker Company</h6>
             <h2 className="product-title">Fall Limited Edition Sneakers</h2>
             <p className="product-description">
                 These low profile sneakers are your perfect casual wear campanion. Featuring a durable rubber outer sole, they'll withstand everything the weather can offer.
             </p>
-            <div className="row">
-                <h4 className="discount-prize">$125.00</h4>
+            <div className="row align-items-center">
+                <h4 className="discount-price">$125.00</h4>
                 <p className="discount-badge">50%</p>
             </div>
-            <h6 className="original-prize">$250.00</h6>
-            <div className="row justify-content-between">
+            <h6 className="original-price">$250.00</h6>
+            <div className="cart-action row justify-content-between align-items-center">
                 <div className="cart-counter-box d-flex align-items-center justify-content-between">
                     <img className='cursor-pointer' src={minusImage} alt="minus-icon" />
                     <span className='product-count'>0</span>
@@ -23,8 +23,8 @@ function ProductDetails() {
                 </div>
                 <div className="add-to-cart-box">
                     <button className='btn btn-primary w-100 d-flex align-items-center justify-content-center'>
-                        <img src={cartImage} alt="cart" />
-                        <span>Add to cart</span>
+                        <img src={cartImage} alt="cart" className='cart-image' />
+                        <span className='cart-btn-text'>Add to cart</span>
                     </button>
                 </div>
             </div>
--- src/components/product-page/ProductImage.tsx
+++ src/components/product-page/ProductImage.tsx
@@ -1,10 +1,30 @@
-import productImage from '../../assets/images/image-product-1.jpg'
+import { useState } from 'react';
+import { ProductImages } from './productImages';
+
 function ProductImage() {
+    const [prodIndex, setProdIndex] = useState(0);
+    const { image } = ProductImages[prodIndex]
     return (
-        <figure>
-            <img src={productImage} alt="product-1" className='product-image' />
-        </figure>
+        <section>
+            <figure>
+                <img src={image} alt="product-1" className='product-image' />
+            </figure>
+            <div className='thumbnail-container'>
+                <ul className='row justify-content-between'>
+                    {ProductImages.map((img, index) => {
+                        return (
+                            <li
+                                key={index}
+                                onClick={() => setProdIndex(index)}
+                            >
+                                <img src={img.thumbnail} alt={`product-thumbnail-${img.id}`} className={`${prodIndex === index ? "active-thumb" : ""} cursor-pointer product-thumbnail`} />
+                            </li>
+                        )
+                    })}
+                </ul>
+            </div>
+        </section>
     )
-}
+};
 
 export default ProductImage;
