9835f1cdf4538cad7a945dc803388de52c080967
Tanmay Patel <tanmay.1rivet@gmail.com>
2023-02-02 19:55:32 +0530

    feat: light box

--- src/components/product-page/ProductImage.tsx
+++ src/components/product-page/ProductImage.tsx
@@ -1,29 +1,58 @@
 import { useState } from 'react';
+
 import { ProductImages } from './productImages';
+import leftArrow from '../../assets/images/icon-previous.svg'
+import rightArrow from '../../assets/images/icon-next.svg'
+import LightBox from './LightBox';
 
 function ProductImage() {
     const [prodIndex, setProdIndex] = useState(0);
     const { image } = ProductImages[prodIndex];
+    const [showLightbox, setShoeLightbox] = useState(false);
+
+    const prevSlide = () => {
+        setProdIndex(prev => prev > 0 ? prev - 1 : 3)
+    }
+
+    const nextslide = () => {
+        setProdIndex(prev => prev < 3 ? prev + 1 : 0)
+    }
     return (
-        <section>
-            <figure>
-                <img src={image} alt="product-1" className='product-image' />
-            </figure>
-            <div className='thumbnail-container'>
-                <ul className='row justify-content-between'>
-                    {ProductImages.map((img, index) => {
-                        return (
-                            <li
-                                key={index}
-                                onClick={() => setProdIndex(index)}
-                            >
-                                <img src={img.thumbnail} alt={`product-thumbnail-${img.id}`} className={`${prodIndex === index ? "active-thumb" : ""} cursor-pointer product-thumbnail`} />
-                            </li>
-                        )
-                    })}
-                </ul>
-            </div>
-        </section>
+        <>
+            {showLightbox && <LightBox
+                ProductImages={ProductImages}
+                prevSlide={prevSlide}
+                nextslide={nextslide}
+                leftArrow={leftArrow}
+                rightArrow={rightArrow}
+                setShoeLightbox={setShoeLightbox}
+            />}
+            <section>
+                <figure className='position-relative'>
+                    <img src={image} alt="product-1" className='product-image cursor-pointer' onClick={() => setShoeLightbox(true)} />
+                    <img src={leftArrow} alt="left-slide-arrow" className='position-absolute slider-arrow cursor-pointer left-arrow'
+                        onClick={() => prevSlide()}
+                    />
+                    <img src={rightArrow} alt="right-slide-arrow" className='position-absolute slider-arrow cursor-pointer right-arrow'
+                        onClick={() => nextslide()}
+                    />
+                </figure>
+                <div className='thumbnail-container'>
+                    <ul className='row justify-content-between'>
+                        {ProductImages.map((img, index) => {
+                            return (
+                                <li
+                                    key={index}
+                                    onClick={() => setProdIndex(index)}
+                                >
+                                    <img src={img.thumbnail} alt={`product-thumbnail-${img.id}`} className={`${prodIndex === index ? "active-thumb" : ""} cursor-pointer product-thumbnail`} />
+                                </li>
+                            )
+                        })}
+                    </ul>
+                </div>
+            </section>
+        </>
     )
 };
 
